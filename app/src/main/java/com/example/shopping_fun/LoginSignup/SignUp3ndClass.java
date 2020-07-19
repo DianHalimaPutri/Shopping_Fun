62356cb691a30f7e5472d0c596af00a4be20e9b2
DianHalimaPutri <63131655+DianHalimaPutri@users.noreply.github.com>
2020-07-19 22:41:58 +0700

    Tugas Besar

--- app/src/main/java/com/example/shopping_fun/LoginSignup/SignUp3ndClass.java
+++ app/src/main/java/com/example/shopping_fun/LoginSignup/SignUp3ndClass.java
@@ -3,23 +3,31 @@ package com.example.shopping_fun.LoginSignup;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.shopping_fun.R;
+import com.google.android.material.textfield.TextInputLayout;
+import com.hbb20.CountryCodePicker;
 
 class SignUp3ndClass extends AppCompatActivity {
     //Variables
     ImageView backBtn;
     Button next, login;
     TextView titleView;
+    ScrollView scrollView;
+    TextInputLayout phoneNumber;
+    CountryCodePicker countryCodePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_sign_up3nd_class);
 
         //Hooks
@@ -27,7 +35,15 @@ class SignUp3ndClass extends AppCompatActivity {
         next = findViewById(R.id.signup_next_button);
         login = findViewById(R.id.signup_login_button);
         titleView = findViewById(R.id.signup_title_text);
+
+       
+        countryCodePicker = findViewById(R.id.country_code_picker);
+        phoneNumber = findViewById(R.id.signup_phone_number);
+
     }
+
+    public void callVerifyOTPScreen (View view){}
+
     public void callNextSignupScreen(View view) {
 
         Intent intent = new Intent(getApplicationContext(),SignUp3ndClass.class);
